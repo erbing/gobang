e3e426cf49b1f85fc62f5504431a20459356ee3c
SmallW <447587073@qq.com>
2019-02-17 22:06:34 +0800

    change app

--- app.js
+++ app.js
@@ -173,11 +173,95 @@ canvas.onclick = e => {
         }
       }
     }
-    // if (!isOver) {
-    //   self = !self;
-    //   computerAI();
-    // }
+    if (!isOver) {
+      self = !self;
+      computerAI();
+    }
   }
 };
 
-const computerAI = () => {};
+const computerAI = () => {
+  var myScore = [];
+  var computerScore = [];
+  var max = 0;
+  var u = 0,
+    v = 0;
+  for (var i = 0; i < 9; i++) {
+    myScore[i] = [];
+    computerScore[i] = [];
+    for (var j = 0; j < 9; j++) {
+      myScore[i][j] = 0;
+      computerScore[i][j] = 0;
+    }
+  }
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+      if (chessBoard[i][j] == 0) {
+        for (var k = 0; k < count; k++) {
+          if (winPaths[i][j][k]) {
+            if (myWin[k] == 1) {
+              myScore[i][j] += 200;
+            } else if (myWin[k] == 2) {
+              myScore[i][j] += 400;
+            } else if (myWin[k] == 3) {
+              myScore[i][j] += 2000;
+            } else if (myWin[k] == 4) {
+              myScore[i][j] += 10000;
+            }
+
+            if (computerWin[k] == 1) {
+              computerScore[i][j] += 220;
+            } else if (computerWin[k] == 2) {
+              computerScore[i][j] += 420;
+            } else if (computerWin[k] == 3) {
+              computerScore[i][j] += 2100;
+            } else if (computerWin[k] == 4) {
+              computerScore[i][j] += 20000;
+            }
+          }
+        }
+
+        if (myScore[i][j] > max) {
+          max = myScore[i][j];
+          u = i;
+          v = j;
+        } else if (myScore[i][j] == max) {
+          if (computerScore[i][j] > computerScore[u][v]) {
+            u = i;
+            v = j;
+          }
+        }
+
+        if (computerScore[i][j] > max) {
+          max = computerScore[i][j];
+          u = i;
+          v = j;
+        } else if (computerScore[i][j] == max) {
+          if (myScore[i][j] > myScore[u][v]) {
+            u = i;
+            v = j;
+          }
+        }
+      }
+    }
+  }
+  var _compi = u;
+  var _compj = v;
+  drawChess(u, v, false);
+  chessBoard[u][v] = 2; //计算机占据位置
+  for (var k = 0; k < count; k++) {
+    if (winPaths[u][v][k]) {
+      computerWin[k]++;
+      //   _myWin[k] = myWin[k];
+      myWin[k] = 6; //这个位置对方不可能赢了
+      if (computerWin[k] == 5) {
+        console.log("o(╯□╰)o，计算机赢了，继续加油哦！");
+        isOver = true;
+      }
+    }
+  }
+  if (!isOver) {
+    console.log(123);
+    // self = !self;
+  }
+};
